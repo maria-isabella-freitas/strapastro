2191d4e0ceb2f6eb7c087a6653dcfd85a879caea
freitas <isabella.nunes.de.freitas@student.howest.be>
2025-12-02 15:01:41 +0100

    project organization chages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["fakestoreapi.com", "picsum.photos"],
+    domains: ["localhost"],
     layout: "constrained",
     responsiveStyles: true,
   },
